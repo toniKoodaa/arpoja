79374354f778e307e1e139c2a6f21f67d3a9f5b1
Toni Virtanen <toniKoodaa@gmail.com>
2021-02-10 16:06:15 +0200

    Building first item
    Basic random number between user given values. Some issues with building form dynamically.

--- client/client.js
+++ client/client.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+
+function GetNumbersFromUser() {
+    const form = document.createElement("form");
+    form.setAttribute("id", "numbersForm");
+    form.setAttribute("method", "get");
+    const firstNumber = document.createElement("input");
+    firstNumber.setAttribute("type", "number");
+    firstNumber.setAttribute("name", "firstNumber");
+    firstNumber.setAttribute("placeholder", "Mista");
+    const secondNumber = document.createElement("input");
+    secondNumber.setAttribute("type", "number");
+    secondNumber.setAttribute("name", "secondNumber")
+    secondNumber.setAttribute("placeholder", "Mihin");
+    const submit = document.createElement("input");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "Submit");
+
+    form.appendChild(firstNumber);
+    form.appendChild(secondNumber);
+    form.appendChild(submit);
+
+    document.querySelector("#template").appendChild(form);
+}
+
+function GetValues() {
+    console.log("toimiiko");
+}
