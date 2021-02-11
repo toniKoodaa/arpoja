6d72d55289c234e0f3e0ec3df7efd690aa4e9b93
Toni Virtanen <toniKoodaa@gmail.com>
2021-02-11 10:33:04 +0200

    little changes
    Added barebones backend and altered design of front end

--- client/client.js
+++ client/client.js
@@ -1,27 +1,10 @@
+const form = document.querySelector('form');
 
-function GetNumbersFromUser() {
-    const form = document.createElement("form");
-    form.setAttribute("id", "numbersForm");
-    form.setAttribute("method", "get");
-    const firstNumber = document.createElement("input");
-    firstNumber.setAttribute("type", "number");
-    firstNumber.setAttribute("name", "firstNumber");
-    firstNumber.setAttribute("placeholder", "Mista");
-    const secondNumber = document.createElement("input");
-    secondNumber.setAttribute("type", "number");
-    secondNumber.setAttribute("name", "secondNumber")
-    secondNumber.setAttribute("placeholder", "Mihin");
-    const submit = document.createElement("input");
-    submit.setAttribute("type", "submit");
-    submit.setAttribute("value", "Submit");
-
-    form.appendChild(firstNumber);
-    form.appendChild(secondNumber);
-    form.appendChild(submit);
-
-    document.querySelector("#template").appendChild(form);
-}
-
-function GetValues() {
-    console.log("toimiiko");
-}
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const firstNumber = formData.get('first');
+    const secondNumber = formData.get('second');
+    console.log("First number: ", firstNumber);
+    console.log("Second number: ", secondNumber);
+})
