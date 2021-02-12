18c8c36bbb70646ee78a619d1e07970b111ddc8b
Toni Virtanen <toniKoodaa@gmail.com>
2021-02-12 10:07:38 +0200

    Working draw between two numbers
    Working draw between two numbers. Still needs some UI changes

--- client/client.js
+++ client/client.js
@@ -1,5 +1,8 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-function GetNumbersFromUser() {
+function GetRandomNumberBetween() {
     const form = document.createElement("form");
     form.setAttribute("id", "numbersForm");
     form.setAttribute("method", "get");
@@ -11,17 +14,25 @@ function GetNumbersFromUser() {
     secondNumber.setAttribute("type", "number");
     secondNumber.setAttribute("name", "secondNumber")
     secondNumber.setAttribute("placeholder", "Mihin");
-    const submit = document.createElement("input");
-    submit.setAttribute("type", "submit");
-    submit.setAttribute("value", "Submit");
+    const button = document.createElement("button");
+    button.innerHTML = "Submit";
 
     form.appendChild(firstNumber);
     form.appendChild(secondNumber);
-    form.appendChild(submit);
+    form.appendChild(button);
 
     document.querySelector("#template").appendChild(form);
-}
 
-function GetValues() {
-    console.log("toimiiko");
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const min = Number.parseInt(formData.get('firstNumber'));
+        const max = Number.parseInt(formData.get('secondNumber'));
+
+        const resultNumber = getRndInteger(min, max + 1);
+        const resultText = document.createElement("h1");
+        resultText.setAttribute("id", "resultH1");
+        resultText.innerText = `${resultNumber}`; 
+        form.appendChild(resultText);
+    })
 }
