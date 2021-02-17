8beb4cd846c8303f9d150e488ff7b62e16efb640
Toni Virtanen <toniKoodaa@gmail.com>
2021-02-17 11:45:40 +0200

    update css, html, javascript
    -Changed UI layout
    -Added different dices to randomise

--- client/client.js
+++ client/client.js
@@ -1,28 +1,58 @@
 
 // Create or select needed elements
-const template = document.querySelector("#template");
-const resultText = document.createElement("h1");
-resultText.setAttribute("id", "resultH1");
+const templateElm = document.querySelector("#template");
+const formsElm = document.querySelector("#forms");
+const resultElm = document.querySelector("#results");
+const selectorsElm = document.querySelector("#selectors");
 
 // get pseudorandom between min and max
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function buttonMaker(text, doWhenClicked) {
+    const button = document.createElement('button');
+    button.setAttribute("id", "selectOne");
+    button.setAttribute("onClick", doWhenClicked);
+    button.innerText = text.toString();
+    return button;
+}
+
+function numbersPage() {
+    templateElm.innerHTML = "";
+    formsElm.innerHTML = "";
+    resultElm.innerHTML = "";
+    const buttonOne = buttonMaker("Arvo numero 1 - 2 välillä", "GetRandomBetweenNumbers(2)");
+    const buttonTwo = buttonMaker("Arvo numero 1 - 10 välillä", "GetRandomBetweenNumbers(10)");
+    const buttonThree = buttonMaker("Arvo numero 1 - 100 välillä", "GetRandomBetweenNumbers(100)");
+    const buttonFore = buttonMaker("Arvo numero kahden luvun välillä", "GetRandomBetweenUserGiven()");
+    templateElm.append(buttonOne, buttonTwo, buttonThree, buttonFore);
+}
+
+function dicesPage() {
+    templateElm.innerHTML = "";
+    formsElm.innerHTML = "";
+    resultElm.innerHTML = "";
+    const buttonOne = buttonMaker("1D4", "GetRandomBetweenNumbers(4)");
+    const buttonTwo = buttonMaker("1D6", "GetRandomBetweenNumbers(6)");
+    const buttonThree = buttonMaker("1D8", "GetRandomBetweenNumbers(8)");
+    const buttonFore = buttonMaker("1D10", "GetRandomBetweenNumbers(10)");
+    const buttonFive = buttonMaker("1D12", "GetRandomBetweenNumbers(12)");
+    const buttonSix = buttonMaker("1D20", "GetRandomBetweenNumbers(20)");
+    templateElm.append(buttonOne, buttonTwo, buttonThree, buttonFore, buttonFive, buttonSix);
+}
+
 // get pseudorandom values for common random picks (like 1-10 and 1-100)
-function GetRandomNumberBetweenNumbers(max) {
-    template.innerHTML = "";
+function GetRandomBetweenNumbers(max) {
     let min = 1;
     const resultNumber = getRndInteger(min, max + 1);
-    resultText.setAttribute("id", "resultH1");
-    resultText.innerText = `${resultNumber}`; 
-    template.appendChild(resultText);
+    resultElm.innerText = `${resultNumber}`; 
 }
 
 // Return pseudorandom from user given range - form is dynamically created
-function GetRandomNumberBetween() {
-    template.innerHTML = "";
-    
+function GetRandomBetweenUserGiven() {
+    formsElm.innerHTML = "";
+
     const form = document.createElement("form");
     form.setAttribute("id", "numbersForm");
     form.setAttribute("method", "get");
@@ -47,7 +77,7 @@ function GetRandomNumberBetween() {
     form.appendChild(secondNumber);
     form.appendChild(button);
 
-    template.appendChild(form);
+    formsElm.appendChild(form);
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -56,8 +86,6 @@ function GetRandomNumberBetween() {
         const max = Number.parseInt(formData.get('secondNumber'));
 
         const resultNumber = getRndInteger(min, max + 1);
-        resultText.setAttribute("id", "resultH1");
-        resultText.innerText = `${resultNumber}`; 
-        form.appendChild(resultText);
+        resultElm.innerText = `${resultNumber}`; 
     })
 }
